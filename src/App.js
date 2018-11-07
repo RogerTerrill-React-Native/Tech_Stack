27f60be2dd83ac301fe1caad6345b562c0c79292
Roger Terrill <rchicasterill@gmail.com>
2018-11-07 10:58:41 -0800

    Rending the Header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common'
 
 const App = () => {
   return (
+    // Provider can only have one child component so you have to nest it
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
     </Provider>
   );
 };
